042121e8bebe30f7cccfae24dbb454edf07af073
Ashwani Yadav <ashyadavash@gmail.com>
2020-04-30 19:58:04 +0530

    added prod built

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const graphqlHttp = require('express-graphql')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const { DB_URI } = require('./configs/config')
 const graphqlSchema = require('./graphql/schema/index')
@@ -46,6 +47,13 @@ app.use('/graphql', graphqlHttp({
     graphiql: true
 }))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 4500
 server.listen(PORT, () => {
--- configs/config.js
+++ configs/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports={
     JWT_SECRET: process.env.JWT_SEC,
-    DB_URI: process.env.DB_URI,
+    DB_URI: process.env.MONGODB_URI,
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
 }
